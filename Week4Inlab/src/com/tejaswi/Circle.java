d1d82fb9165ad1abb797ed8b0bdeab7ace410d98
PHANI TEJASWI <ptejaswi42@gmail.com>
2024-08-15 14:51:55 +0530

    Add files via upload

---
+++ Week4Inlab/src/com/tejaswi/Circle.java
@@ -0,0 +1,19 @@
+package com.tejaswi;
+
+public class Circle extends Shape
+{
+private int radius;
+private int x;
+private int y;
+public Circle(int radius, int x, int y, DrawAPI drawAPI)
+{
+super(drawAPI);
+this.radius = radius;
+this.x = x;
+this.y = y;
+}
+public void draw()
+{
+drawAPI.drawCircle(radius, x, y);
+}
+}
---
+++ Week4Inlab/src/com/tejaswi/GreenCircle.java
@@ -0,0 +1,11 @@
+package com.tejaswi;
+
+public class GreenCircle  implements DrawAPI
+{
+public void drawCircle(int radius, int x,int y)
+{
+System.out.println("Drawing green circle with radius "+radius+" at ("+x+ ","+y+ ")");
+}
+
+
+}
