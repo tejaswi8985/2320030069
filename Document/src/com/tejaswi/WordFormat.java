45570dcb5fd20380daae3bd26113293450f8f3a2
PHANI TEJASWI <ptejaswi42@gmail.com>
2024-08-18 21:14:18 +0530

    Update WordFormat.java

--- Document/src/com/tejaswi/WordFormat.java
+++ Document/src/com/tejaswi/WordFormat.java
@@ -1,6 +1,7 @@
 package com.tejaswi;
 
 public class WordFormat implements DocumentFormat {
+	  @Override
 	public void generateDocument(String content, String filePath) {
         System.out.println("Generating Word document at: " + filePath);
     }
