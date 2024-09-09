5c695098cd27c4531cb210de57b76ca4d856b15a
PHANI TEJASWI <ptejaswi42@gmail.com>
2024-09-09 23:50:25 +0530

    Add files via upload

---
+++ week2skill/src/week2skill/OnlineStreamMusic.java
@@ -0,0 +1,5 @@
+package week2skill;
+
+public class OnlineStreamMusic {
+
+}
---
+++ week2skill/src/week2skill/VolumeControlDecorator.java
@@ -0,0 +1,17 @@
+package week2skill;
+
+public class VolumeControlDecorator extends MusicSourceDecorator {
+	public VolumeControlDecorator(MusicSource decoratedMusicSource) {
+        super(decoratedMusicSource);
+    }
+
+    public void play() {
+        super.play();
+        setVolume();
+    }
+
+    private void setVolume() {
+        System.out.println("Volume control settings applied.");
+    }
+
+}
