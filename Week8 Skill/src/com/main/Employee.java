45867c643a6a1d1ca0e3289726dc0d306b6891a4
PHANI TEJASWI <ptejaswi42@gmail.com>
2024-09-10 11:00:48 +0530

    Add files via upload

---
+++ Week8 Skill/src/com/main/Employee.java
@@ -0,0 +1,38 @@
+package com.main;
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 && name.equals(employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{name='" + name + "', salary=" + salary + '}';
+    }
+}
