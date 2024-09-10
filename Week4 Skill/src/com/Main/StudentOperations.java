841fddb16077d38c25cdfd3f9a8265a8ee22071e
PHANI TEJASWI <ptejaswi42@gmail.com>
2024-09-10 11:01:33 +0530

    Add files via upload

---
+++ Week4 Skill/src/com/Main/StudentOperations.java
@@ -0,0 +1,10 @@
+package com.Main;
+
+import java.util.List;
+
+public interface StudentOperations {
+    String getName();
+    String getId();
+    void enrollCourse(Course course);
+    List<Course> getCourses();
+}
