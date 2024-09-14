88a996b8f7b176595daca47ea0f89db87a20418b
PHANI TEJASWI <ptejaswi42@gmail.com>
2024-09-14 13:37:56 +0530

    Delete Week4 Skill directory

--- Week4 Skill/src/com/Main/EnrollmentService.java
+++
@@ -1,8 +0,0 @@
-package com.Main;
-
-public class EnrollmentService {
-    public void enroll(Student student, Course course) {
-        student.enrollCourse(course);
-        course.enrollStudent(student);
-    }
-}
