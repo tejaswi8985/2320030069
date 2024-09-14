8915cf45bd27144b4350803c7f73f611e3552ed9
PHANI TEJASWI <ptejaswi42@gmail.com>
2024-09-14 13:42:02 +0530

    Delete week5postlab directory

--- week5postlab/src/week5postlab/AuctionItem.java
+++
@@ -1,5 +0,0 @@
-package week5postlab;
-
-public class AuctionItem {
-
-}
--- week5postlab/src/week5postlab/Subject.java
+++
@@ -1,7 +0,0 @@
-package week5postlab;
-
-public interface Subject {
-	 void addObserver(Observer observer);
-	    void removeObserver(Observer observer);
-	    void notifyObservers(String event);
-}
